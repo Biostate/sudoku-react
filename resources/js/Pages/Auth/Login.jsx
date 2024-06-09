a06482dfd32d7fd1ece95ae171bbaef60a8f9fde
Süleyman Özgür Özarpacı <oozarpac@gmail.com>
2024-06-10 01:02:55 +0300

    add register to login

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -86,6 +86,13 @@ export default function Login({ status, canResetPassword }) {
                             Forgot your password?
                         </Link>
                     )}
+                    &nbsp;or&nbsp;
+                    <Link
+                        href={route('register')}
+                        className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 me-4"
+                    >
+                        Register
+                    </Link>
 
                     <PrimaryButton className="ms-4" disabled={processing}>
                         Log in
